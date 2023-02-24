03723bb4b319c99c9bfd5041a0e8c619d430dc4d
Ari Ketola <ariketola@hotmail.com>
2023-02-24 08:58:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IDP_assg_3
+{
+    internal class Program
+    {
+
+        class Grundämne
+        {
+
+            public string namn, typ;
+            public int z;
+            public double ismältpunkt, kokpunkt;
+
+            public void Print()
+            {
+                Console.Write(" " + namn + "  ");
+                Console.Write($"{typ} ");
+                Console.WriteLine(typ + "  " + typ);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+ 
+
+
+
+    }
+    }
+}
