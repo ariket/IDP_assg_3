cb0701da547ed69c8674089b55a627355a9ff60d
Ari Ketola <ariketola@hotmail.com>
2023-02-24 09:29:16 +0100

    skapat en foreach loop som skriver ut hela arrayen

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace IDP_assg_3
+﻿using System.Reflection;
+
+namespace IDP_assg_3
 {
     internal class Program
     {
@@ -27,17 +29,22 @@
             Grundämne syre = new Grundämne() { namn = "syre", z = 8, typ = "ickemetall", smältpunkt = 54.36, kokpunkt = 90.188};
             Grundämne järn = new Grundämne() { namn = "järn", z = 26, typ = "metall", smältpunkt = 1811, kokpunkt = 3134};
             Grundämne guld = new Grundämne() { namn = "guld", z = 79, typ = "metall", smältpunkt = 1337.33, kokpunkt = 3243};
-
-            syre.Print();
-            järn.Print();
-            guld.Print();
+            
+            //Uppgift 3.
+            //syre.Print();
+            //järn.Print();
+            //guld.Print();
 
 
             Grundämne[] Grundämnen = new Grundämne[6] { syre, järn, guld,
             new Grundämne() { namn = "väte", z = 1, typ = "ickemetall", smältpunkt = 13.99, kokpunkt = 20.271 },
             new Grundämne() { namn = "brom", z = 35, typ = "ickemetall", smältpunkt = 265.8, kokpunkt = 332.0 },
             new Grundämne() { namn = "kvicksilver", z = 80, typ = "metall", smältpunkt = 234.321, kokpunkt = 629.88 }};
-            
+
+            foreach (Grundämne xx in Grundämnen)
+            {
+                xx.Print();
+            }
 
         }
     }
