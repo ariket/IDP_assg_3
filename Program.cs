ec91ef684833493d81cb4c63612b95414e775be3
Ari Ketola <ariketola@hotmail.com>
2023-02-24 09:10:23 +0100

    Lagt till tre grundämnen

--- Program.cs
+++ Program.cs
@@ -24,6 +24,8 @@
         {
 
             Grundämne syre = new Grundämne() { namn = "syre", z = 8, typ = "ickemetall", smältpunkt = 54.36, kokpunkt = 90.188};
+            Grundämne järn = new Grundämne() { namn = "järn", z = 26, typ = "metall", smältpunkt = 1811, kokpunkt = 3134};
+            Grundämne guld = new Grundämne() { namn = "guld", z = 79, typ = "metall", smältpunkt = 1337.33, kokpunkt = 3243};
 
 
 
