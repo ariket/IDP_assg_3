9c1e24340332c545797b541597bbb4d447e9c04f
Ari Ketola <ariketola@hotmail.com>
2023-02-24 09:55:36 +0100

    Skrivit rubriker som skrivs ut i programmet till varje övningsuppgift

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 namespace IDP_assg_3
 {
     //Inlämningsuppgift IDP_assg_3  2023-02-24
-    //Enkelt program som hanter grundämnen
+    //Enkelt program som hanterar grundämnen
     internal class Program
     {
 
@@ -31,32 +31,32 @@ namespace IDP_assg_3
             Grundämne järn = new Grundämne() { namn = "järn", z = 26, typ = "metall", smältpunkt = 1811, kokpunkt = 3134 };
             Grundämne guld = new Grundämne() { namn = "guld", z = 79, typ = "metall", smältpunkt = 1337.33, kokpunkt = 3243 };
 
-            //Uppgift 3.
-            //syre.Print();
-            //järn.Print();
-            //guld.Print();
-
+            Console.WriteLine("Uppgift 3: ");
+            syre.Print();
+            järn.Print();
+            guld.Print();
+            Console.WriteLine(" ");
 
             Grundämne[] Grundämnen = new Grundämne[6] { syre, järn, guld,
             new Grundämne() { namn = "väte", z = 1, typ = "ickemetall", smältpunkt = 13.99, kokpunkt = 20.271 },
             new Grundämne() { namn = "brom", z = 35, typ = "ickemetall", smältpunkt = 265.8, kokpunkt = 332.0 },
             new Grundämne() { namn = "kvicksilver", z = 80, typ = "metall", smältpunkt = 234.321, kokpunkt = 629.88 }};
 
-            //Uppgift 5
-            //foreach (Grundämne xx in Grundämnen)
-            //{
-            //    xx.Print();
-            //}
+            Console.WriteLine("Uppgift 5: ");
+            foreach (Grundämne xx in Grundämnen)
+            {
+                xx.Print();
+            }
 
-            //Uppgift 6
-            //Console.WriteLine("Metaller:");
-            //foreach (Grundämne xx in Grundämnen)
-            //{
-            //    if (xx.typ == "metall")
-            //    Console.WriteLine(xx.namn);
-            //}
+            Console.WriteLine("\nUppgift 6: ");
+            Console.WriteLine("Metaller:");
+            foreach (Grundämne xx in Grundämnen)
+            {
+                if (xx.typ == "metall")
+                Console.WriteLine(xx.namn);
+            }
 
-
+            Console.WriteLine("\nUppgift 7: ");
             Console.WriteLine("Grundämnen med en smältpunkt under 273.16 K (d.v.s. 0°C) och en kokpunkt över 273.16:");
             foreach (Grundämne xx in Grundämnen)
             {
